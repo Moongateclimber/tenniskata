a62bca211efa09fdd7d6f6b641dca88e28be1ad0
Andrea Sosio <andrea.sosio@yahoo.it>
2017-02-10 15:06:37 +0100

    more separation of concerns, stepdown rule

--- src/org/tensoc/game/TennisGame1.java
+++ src/org/tensoc/game/TennisGame1.java
@@ -21,7 +21,7 @@ class Player {
 	}
 }
 
-public class TennisGame1 implements TennisGame {
+class TennisTerms {
 	public static final String FORTY = "Forty";
 	public static final String WIN_FOR = "Win for";
 	public static final String ADVANTAGE = "Advantage";
@@ -30,6 +30,10 @@ public class TennisGame1 implements TennisGame {
 	public static final String FIFTEEN = "Fifteen";
 	public static final String ALL = "All";
 	public static final String LOVE = "Love";
+}
+
+public class TennisGame1 implements TennisGame {
+	public static final int LAST_POINT_BEFORE_ADVANTAGE_SCORING = 3;
 
 	Player players[] = new Player[2];
 
@@ -51,64 +55,72 @@ public class TennisGame1 implements TennisGame {
 
 	public String getScore() {
 		if (isDeuce())
-			if (lessThanForty(players[0].getPointsWon()))
-				return getNamesForBasicScore(players[0].getPointsWon()) + "-"
-						+ ALL;
-			else
-				return DEUCE;
+			return getDeuceScore();
 
-		if (moreThanForty(players[0].getPointsWon())
-				|| moreThanForty(players[1].getPointsWon()))
-			for (int i = 0; i < 2; i++) {
-				if (onePointAdvantage(players[i]))
-					return ADVANTAGE + " " + players[i].getName();
-				if (moreThanOnePointAdvantage(players[i]))
-					return WIN_FOR + " " + players[i].getName();
-			}
+		if (isAdvantage())
+			return getAdvantageScore();
 
+		return getBasicScore();
+	}
+
+	protected boolean isAdvantage() {
+		return players[0].getPointsWon() > TennisGame1.LAST_POINT_BEFORE_ADVANTAGE_SCORING
+				|| players[1].getPointsWon() > TennisGame1.LAST_POINT_BEFORE_ADVANTAGE_SCORING;
+	}
+
+	protected boolean isDeuce() {
+		return players[0].getPointsWon() == players[1].getPointsWon();
+	}
+
+	private String getDeuceScore() {
+		if (players[0].getPointsWon() < TennisGame1.LAST_POINT_BEFORE_ADVANTAGE_SCORING)
+			return getNamesForBasicScore(players[0].getPointsWon()) + "-"
+					+ TennisTerms.ALL;
+		else
+			return TennisTerms.DEUCE;
+	}
+
+	private String getAdvantageScore() {
+		for (int i = 0; i < 2; i++) {
+			if (onePointAdvantage(players[i]))
+				return TennisTerms.ADVANTAGE + " " + players[i].getName();
+			if (moreThanOnePointAdvantage(players[i]))
+				return TennisTerms.WIN_FOR + " " + players[i].getName();
+		}
+		return null;
+	}
+
+	protected String getBasicScore() {
 		return getNamesForBasicScore(players[0].getPointsWon()) + "-"
 				+ getNamesForBasicScore(players[1].getPointsWon());
 	}
 
-	protected boolean moreThanOnePointAdvantage(Player player) {
-		return player.getPointsWon() > opponent(player)
-				.getPointsWon() + 1;
+	private boolean moreThanOnePointAdvantage(Player player) {
+		return player.getPointsWon() > opponent(player).getPointsWon() + 1;
 	}
 
-	protected boolean onePointAdvantage(Player player) {
+	private boolean onePointAdvantage(Player player) {
 		return player.getPointsWon() == opponent(player).getPointsWon() + 1;
 	}
 
 	private String getNamesForBasicScore(int score) {
 		switch (score) {
 		case 0:
-			return LOVE;
+			return TennisTerms.LOVE;
 		case 1:
-			return FIFTEEN;
+			return TennisTerms.FIFTEEN;
 		case 2:
-			return THIRTY;
+			return TennisTerms.THIRTY;
 		case 3:
-			return FORTY;
+			return TennisTerms.FORTY;
 		default:
 			return "Unnamed score";
 		}
 	}
 
-	private boolean isDeuce() {
-		return players[0].getPointsWon() == players[1].getPointsWon();
-	}
-
-	private boolean lessThanForty(int pointsWon) {
-		return pointsWon < 3;
-	}
-
-	private boolean moreThanForty(int pointsWon) {
-		return pointsWon >= 4;
-	}
-
 	private Player opponent(Player player) {
-		for(int i=0; i<2; i++)
-			if(players[i]!=player)
+		for (int i = 0; i < 2; i++)
+			if (players[i] != player)
 				return players[i];
 		return null;
 	}
