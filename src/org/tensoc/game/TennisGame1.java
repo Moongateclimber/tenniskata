d8b1fa8615d39f035ca2a1eff71725ac0ab678c7
Andrea Sosio <andrea.sosio@yahoo.it>
2017-02-10 13:51:35 +0100

    turning symmetric independent vars into arrays (will support DRY later) - playerNames

--- src/org/tensoc/game/TennisGame1.java
+++ src/org/tensoc/game/TennisGame1.java
@@ -11,18 +11,17 @@ public class TennisGame1 implements TennisGame {
 	public static final String LOVE = "Love";
 
 	private int pointsWonByPlayer[] = new int[2];
-	private String player1Name;
-	private String player2Name;
+	private String playerNames[] = new String[2];
 
 	public TennisGame1(String player1Name, String player2Name) {
-		this.player1Name = player1Name;
-		this.player2Name = player2Name;
+		this.playerNames[0] = player1Name;
+		this.playerNames[1] = player2Name;
 	}
 
 	public void wonPoint(String playerName) {
-		if (playerName == player1Name)
+		if (playerName == playerNames[0])
 			pointsWonByPlayer[0] += 1;
-		else if (playerName == player2Name)
+		else if (playerName == playerNames[1])
 			pointsWonByPlayer[1] += 1;
 	}
 
@@ -55,19 +54,17 @@ public class TennisGame1 implements TennisGame {
 				return getNamesForBasicScore(pointsWonByPlayer[0])+"-"+ALL;
 			else
 				return DEUCE;
-		
-		
 
 		if (pointsWonByPlayer[0] >= 4 || pointsWonByPlayer[1] >= 4) {
 			int minusResult = pointsWonByPlayer[0] - pointsWonByPlayer[1];
 			if (minusResult == 1)
-				return ADVANTAGE + " " + player1Name;
+				return ADVANTAGE + " " + playerNames[0];
 			else if (minusResult == -1)
-				return ADVANTAGE + " " + player2Name;
+				return ADVANTAGE + " " + playerNames[1];
 			else if (minusResult >= 2)
-				return WIN_FOR + " " + player1Name;
+				return WIN_FOR + " " + playerNames[0];
 			else
-				return WIN_FOR + " " + player2Name;
+				return WIN_FOR + " " + playerNames[1];
 		} else {
 			String score = "";
 			int tempScore = 0;
